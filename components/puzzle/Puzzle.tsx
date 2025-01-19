46479d347fe0f41c24429160ffa8e2365067a481
esirango <esirango23@gmail.com>
2025-01-19 15:14:06 +0330

    develope puzzle game

--- components/puzzle/Puzzle.tsx
+++ components/puzzle/Puzzle.tsx
@@ -1,26 +1,46 @@
 import React, { useState } from "react";
 
 import styles from "@/styles/games/puzzle/puzzle.module.css";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+
+import { JigsawPuzzle } from "react-jigsaw-puzzle";
 
 function Puzzle({ images }: any) {
     const [selectedImage, setSelectedImage] = useState<string>("");
     return (
-        <div className={styles.mainPuzzle}>
-            <h1>Puzzle Game</h1>
-
+        <div
+            className={styles.mainPuzzle}
+            style={
+                selectedImage
+                    ? { justifyContent: "center" }
+                    : { justifyContent: "flex-start" }
+            }
+        >
             {selectedImage ? (
-                <></>
+                <div className={styles.jigsawPuzzle}>
+                    <JigsawPuzzle
+                        imageSrc={selectedImage}
+                        rows={5}
+                        columns={5}
+                        onSolved={() => alert("Solved!")}
+                    />
+                    <span onClick={() => setSelectedImage("")}>
+                        Choose another image
+                    </span>
+                </div>
             ) : (
                 <div className={styles.imagesList}>
+                    <h1>Puzzle Game</h1>
                     <h2>Choose the puzzle you want</h2>
-
-                    {images?.map((image: string) => (
-                        <img
-                            src={image}
-                            alt={image}
-                            onClick={() => setSelectedImage(image)}
-                        />
-                    ))}
+                    <div className={styles.image}>
+                        {images?.map((image: string) => (
+                            <img
+                                src={image}
+                                alt={image}
+                                onClick={() => setSelectedImage(image)}
+                            />
+                        ))}
+                    </div>
                 </div>
             )}
         </div>
